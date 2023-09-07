8f2126898db972f75ded4c0b5982fbfc26ea1fcb
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-07 15:01:54 -0500

    added sql directory and created a seeder sql file

---
+++ SQL scripts/seeder.sql
@@ -0,0 +1,28 @@
+use adlister_db;
+
+INSERT INTO users (firstname, lastname, username, email, password)
+VALUES ('jose',
+        'delaluz',
+        'dmoney23',
+        'real@email.com',
+        'password'
+        );
+INSERT INTO users (firstname, lastname, username, email, password)
+VALUES ('Johnny',
+        'Manziel',
+        'JohnnyFootball',
+        'real@email.com',
+        'password'
+       );
+INSERT INTO ads (user_id, title, link, description)
+VALUES (1,
+        'Hello world',
+        'www.myspace.com',
+        'tom is really my friend'
+       );
+INSERT INTO ads (user_id, title, link, description)
+VALUES (2,
+        'can you do a kickflip poser?',
+        'www.coolmathgames.com',
+        'this is a description'
+       );
