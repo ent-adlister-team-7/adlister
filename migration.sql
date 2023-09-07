ac31ab3408bbd247f1fe8db08cc927a209da9b37
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-07 13:43:53 -0500

    migration script for local db is updated

--- migration.sql
+++ migration.sql
@@ -5,6 +5,8 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    firstname varchar(240) not null ,
+    lastname varchar(240) not null,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -15,6 +17,7 @@ CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
+    link varchar(255) not null ,
     description TEXT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
