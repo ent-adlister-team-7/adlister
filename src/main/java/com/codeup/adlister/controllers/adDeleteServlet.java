878481e5720aba57f7648ba0f28d1561e15aaca9
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-11 13:52:21 -0500

    added a delete method c:if in jsp to check credentials and redirects to view all ads

--- src/main/java/com/codeup/adlister/controllers/adDeleteServlet.java
+++ src/main/java/com/codeup/adlister/controllers/adDeleteServlet.java
@@ -6,30 +6,32 @@ import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.AdDeleteServlet", urlPatterns = "/ads/delete")
-public class adDeleteServlet {
+@WebServlet(name = "controllers.AdDeleteServlet", urlPatterns = "/delete")
+public class adDeleteServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") == null) {
-            response.sendRedirect("/login");
-            // add a return statement to exit out of the entire method.
-            return;
-        }
-        // Dispatcher can be changed whenever we get to this, using viewAds.jsp as a placeholder jsp for now will need a  NOT FINAL UNLESS SAID OTHERWISE
+//        if (request.getSession().getAttribute("user") == null) {
+//            response.sendRedirect("/login");
+//            // add a return statement to exit out of the entire method.
+//            return;
+//        }
         request.getRequestDispatcher("/WEB-INF/ads/displayad.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User loggedInUser = (User) request.getSession().getAttribute("user");
-        Ad viewedAd = (Ad) request.getSession().getAttribute("ad");
+        String adId = request.getParameter("adId");
+        long id = Long.parseLong(adId);
+        Ad ad = DaoFactory.getAdsDao().getAdByID(id);
         if (loggedInUser != null) {
-            DaoFactory.getAdsDao().delete(viewedAd);
+            DaoFactory.getAdsDao().delete(ad);
+            response.sendRedirect("/ads");
         }
-        response.sendRedirect("/ads");
     }
 }
 
--- src/main/webapp/WEB-INF/ads/displayAd.jsp
+++ src/main/webapp/WEB-INF/ads/displayAd.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp"/>
@@ -12,6 +13,20 @@
     <h2><a href="${ad.link}" target="_blank">${ad.link}</a></h2>
     <%-- <img src="${ad.imageSource}">--%>
     <p>${ad.description}</p>
+    <c:if test="${not empty sessionScope.user}">
+        <c:choose>
+        <c:when test="${ad.user.id == sessionScope.user.id}">
+            <form action="/delete" method="post">
+                <input type="hidden" name="adId" value="${ad.id}">
+                <input type="submit" value="Delete Ad">
+            </form>
+        </c:when>
+        <c:otherwise>
+            <p>You do not have permission to delete this ad.</p>
+        </c:otherwise>
+    </c:choose>
+    </c:if>
+
 </div>
 <jsp:include page="/WEB-INF/partials/script.jsp" />
 <jsp:include page="/WEB-INF/partials/footer.jsp" />
